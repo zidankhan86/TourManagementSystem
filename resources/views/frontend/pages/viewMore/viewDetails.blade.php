2cf01cd8d5ef697c5a434c43c4a153c136a1fe1e
zidankhan86 <zidankhan718@gmail.com>
2023-05-18 15:26:21 +0600

    View details problem fixed

--- resources/views/frontend/pages/viewMore/viewDetails.blade.php
+++ resources/views/frontend/pages/viewMore/viewDetails.blade.php
@@ -44,8 +44,8 @@ body{
 <div class="container">
     <div class="card">
         <div class="card-body">
-            <h3 class="card-title">Rounded Chair</h3>
-            <h6 class="card-subtitle">globe type chair for rest</h6>
+            <h3 class="card-title"> {{ $toursData->tittle }}</h3>
+            <h6 class="card-subtitle"></h6>
             <div class="row">
                 <div class="col-lg-5 col-md-5 col-sm-6">
                     <div class="white-box text-center"><img height="550px" width="350px" src="{{ url('/uploads/uploads/'.$toursData->image) }}" class="img-responsive"></div>
@@ -66,73 +66,6 @@ body{
                     </ul>
                 </div>
                 <div class="col-lg-12 col-md-12 col-sm-12">
-                    <h3 class="box-title mt-5">General Info</h3>
-                    <div class="table-responsive">
-                        <table class="table table-striped table-product">
-                            <tbody>
-                                <tr>
-                                    <td width="390">Brand</td>
-                                    <td>Stellar</td>
-                                </tr>
-                                <tr>
-                                    <td>Delivery Condition</td>
-                                    <td>Knock Down</td>
-                                </tr>
-                                <tr>
-                                    <td>Seat Lock Included</td>
-                                    <td>Yes</td>
-                                </tr>
-                                <tr>
-                                    <td>Type</td>
-                                    <td>Office Chair</td>
-                                </tr>
-                                <tr>
-                                    <td>Style</td>
-                                    <td>Contemporary&amp;Modern</td>
-                                </tr>
-                                <tr>
-                                    <td>Wheels Included</td>
-                                    <td>Yes</td>
-                                </tr>
-                                <tr>
-                                    <td>Upholstery Included</td>
-                                    <td>Yes</td>
-                                </tr>
-                                <tr>
-                                    <td>Upholstery Type</td>
-                                    <td>Cushion</td>
-                                </tr>
-                                <tr>
-                                    <td>Head Support</td>
-                                    <td>No</td>
-                                </tr>
-                                <tr>
-                                    <td>Suitable For</td>
-                                    <td>Study&amp;Home Office</td>
-                                </tr>
-                                <tr>
-                                    <td>Adjustable Height</td>
-                                    <td>Yes</td>
-                                </tr>
-                                <tr>
-                                    <td>Model Number</td>
-                                    <td>F01020701-00HT744A06</td>
-                                </tr>
-                                <tr>
-                                    <td>Armrest Included</td>
-                                    <td>Yes</td>
-                                </tr>
-                                <tr>
-                                    <td>Care Instructions</td>
-                                    <td>Handle With Care,Keep In Dry Place,Do Not Apply Any Chemical For Cleaning.</td>
-                                </tr>
-                                <tr>
-                                    <td>Finish Type</td>
-                                    <td>Matte</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
                 </div>
             </div>
         </div>
